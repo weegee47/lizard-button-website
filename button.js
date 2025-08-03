7b8a3c388dc282840e8229e4979698c665635c69
weegee47 <atn0714@icloud.com>
2025-08-02 17:10:23 -0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,16 @@
+// varibles for audio
+const lizard = new Audio("/audio/lizard.mp3");
+const buttons = document.querySelectorAll("button");
+
+// varibles for counter
+let count = 0;
+const counter = document.getElementById("counter");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    lizard.play();
+    lizard.currentTime = 0;
+    count++;
+    counter.textContent = count;
+  });
+});
